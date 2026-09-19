8164d238266c786d898f5defdb27ed565b646473
Leobert Camoro <leobert.camoro101@gmail.com>
2026-09-19 13:59:33 +0800

    update cors.php to fix issue

--- taskflow-api/config/cors.php
+++ taskflow-api/config/cors.php
@@ -2,7 +2,7 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))),
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
